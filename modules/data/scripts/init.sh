e2eeca3db9dfdf09f01d52e14a452c330191ab90
Gerd Aschemann <gerd@aschemann.net>
2016-07-06 17:19:10 +0200

    Add Docker

---
+++ modules/data/scripts/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+dir=`dirname $0`
+
+basedir="${dir}/.."
+
+modulename=`dirname $basedir`
+modulename=`dirname $modulename`
+modulename=`basename $modulename`
+cat<<EOM
+======================================================================
+Installing "${modulename}"
+======================================================================
+EOM
+
+sudo=/usr/bin/sudo
+test -x $sudo || sudo=
+
+$sudo puppet apply "$basedir/puppet/init.pp"
